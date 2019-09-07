c102b28a4214bb6d24d01519da87d8f92e57729c
gur111 <gur111@users.noreply.github.com>
2019-09-07 16:31:53 +0300

    Lesson changes and function renames

--- example.c
+++ example.c
@@ -13,33 +13,78 @@
 
 #include <stdio.h>
 #include <pthread.h>
+#include <zconf.h>
 #include "thpool.h"
 
 
 void task1(){
+    printf("Start 1\n");
+    sleep(1);
 	printf("Thread #%u working on task1\n", (int)pthread_self());
 }
 
 
 void task2(){
+    printf("Start 2\n");
+    sleep(1);
 	printf("Thread #%u working on task2\n", (int)pthread_self());
+}
+
+void task3(){
+    printf("Start 3\n");
+    sleep(1);
+	printf("Thread #%u working on task3\n", (int)pthread_self());
+}
+
+void task4(){
+    printf("Start 4\n");
+    sleep(1);
+	printf("Thread #%u working on task4\n", (int)pthread_self());
+}
+
+void task5(){
+    printf("Start 5\n");
+    sleep(1);
+	printf("Thread #%u working on task5\n", (int)pthread_self());
+}
+
+void task6(){
+    printf("Start 6\n");
+    sleep(1);
+	printf("Thread #%u working on task6\n", (int)pthread_self());
+}
+
+void task7(){
+    printf("Start 7\n");
+    sleep(1);
+	printf("Thread #%u working on task7\n", (int)pthread_self());
 }
 
 
 int main(){
 	
-	puts("Making threadpool with 4 threads");
-	threadpool thpool = thpool_init(4);
+	puts("Making threadpool with 2 threads");
+	threadpool thpool = ThreadPoolInit(2);
 
-	puts("Adding 40 tasks to threadpool");
+
 	int i;
-	for (i=0; i<20; i++){
-		thpool_add_work(thpool, (void*)task1, NULL);
-		thpool_add_work(thpool, (void*)task2, NULL);
+	for (i=0; i<2; i++){
+        puts("Adding 7 tasks to threadpool");
+		ThreadPoolInsertTask(thpool, (void*)task1, NULL);
+		ThreadPoolInsertTask(thpool, (void*)task2, NULL);
+		ThreadPoolInsertTask(thpool, (void*)task3, NULL);
+		ThreadPoolInsertTask(thpool, (void*)task4, NULL);
+		ThreadPoolInsertTask(thpool, (void*)task5, NULL);
+		ThreadPoolInsertTask(thpool, (void*)task6, NULL);
+		ThreadPoolInsertTask(thpool, (void*)task7, NULL);
+		sleep(2);
 	};
 
+//    puts("Waiting for all jobs to finish");
+//    thpool_wait(thpool);
+
 	puts("Killing threadpool");
-	thpool_destroy(thpool);
+	ThreadPoolDestroy(thpool);
 	
 	return 0;
 }
